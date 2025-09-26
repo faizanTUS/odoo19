9d9c872bc57f0d454f5c728310fde1dfc66e7e49
riyanTUS <riyanshaikh.techultra@gmail.com>
2025-09-26 16:00:17 +0530

    [ADD] tus_ecommerce_barcode_search odoo19

---
+++ riyan/tus_ecommerce_barcode_search/__manifest__.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+{
+    'name': 'Ecommerce Barcode Search',
+    'version': '19.0.0.0',
+    'category': 'Website',
+    'summary': '''Ecommerce Barcode Search
+    
+    tus
+    techultra
+    techultra_private_limited_solution
+    barcode search ecommerce
+    product search by barcode
+    ecommerce barcode scanner
+    barcode lookup tool
+    online barcode search module
+    search product by barcode number
+    ecommerce barcode lookup
+    barcode scanning plugin
+    barcode inventory search
+    barcode product identifier
+    odoo barcode search module
+    odoo ecommerce barcode scanner
+    odoo product search by barcode
+    barcode lookup in odoo
+    odoo barcode integration
+    odoo ecommerce product scanner
+    odoo barcode inventory search
+    odoo scan barcode to find product
+    barcode search in odoo website
+    odoo website product barcode search
+    odoo barcode product lookup
+    search ecommerce products by barcode odoo
+    odoo product page barcode search
+    odoo barcode scanning ecommerce
+    odoo online store barcode scanner
+    odoo barcode reader for webshop
+    best odoo barcode search app
+    buy odoo barcode scanner module
+    odoo barcode search plugin for ecommerce
+    barcode reader module for odoo ecommerce
+    odoo website barcode search add-on
+    odoo product search optimization barcode
+    how to search products by barcode in odoo website
+    odoo ecommerce extension for barcode search
+    add barcode scanner to odoo online store
+    module to search odoo webshop products by barcode
+    scan barcode to find product on odoo site
+    ''',
+    'description': '''Product search in website using Barcode Odoo
+    
+    tus
+    techultra
+    techultra_private_limited_solution
+    barcode search ecommerce
+    product search by barcode
+    ecommerce barcode scanner
+    barcode lookup tool
+    online barcode search module
+    search product by barcode number
+    ecommerce barcode lookup
+    barcode scanning plugin
+    barcode inventory search
+    barcode product identifier
+    odoo barcode search module
+    odoo ecommerce barcode scanner
+    odoo product search by barcode
+    barcode lookup in odoo
+    odoo barcode integration
+    odoo ecommerce product scanner
+    odoo barcode inventory search
+    odoo scan barcode to find product
+    barcode search in odoo website
+    odoo website product barcode search
+    odoo barcode product lookup
+    search ecommerce products by barcode odoo
+    odoo product page barcode search
+    odoo barcode scanning ecommerce
+    odoo online store barcode scanner
+    odoo barcode reader for webshop
+    best odoo barcode search app
+    buy odoo barcode scanner module
+    odoo barcode search plugin for ecommerce
+    barcode reader module for odoo ecommerce
+    odoo website barcode search add-on
+    odoo product search optimization barcode
+    how to search products by barcode in odoo website
+    odoo ecommerce extension for barcode search
+    add barcode scanner to odoo online store
+    module to search odoo webshop products by barcode
+    scan barcode to find product on odoo site
+        
+    ''',
+    'author': "Techultra Solutions Private Limited",
+    'website': "https://www.techultrasolutions.com/",
+    "price": 9,
+    "currency": "USD",
+    'images': ['static/description/main_screen.gif'],
+    'depends': ['website_sale'],
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
