6869b2492cb05d49686a62bcfa89fa45806b0a4d
riyanTUS <riyanshaikh.techultra@gmail.com>
2025-09-26 11:50:26 +0530

    [ADD] assign_salesperson odoo19

---
+++ riyan/assign_salesperson/__manifest__.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# Part of TechUltra Solutions. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Assign Salesperson",
+    'version': '19.0.0.0',
+    'category': 'CRM',
+    'summary': """
+        Module To Assign Salesperson to multiple leads from wizard.
+        CRM Lead
+        Crm
+        Lead
+        Assign
+        Salesperson
+        Salesperson Assign
+        Assign Salesperson
+        Assign salesperson CRM
+        Bulk assign leads CRM
+        CRM salesperson assignment tool
+        Mass lead reassignment
+        CRM lead management
+        Salesperson lead distribution
+        CRM automation for sales team
+        Lead assignment CRM
+        CRM sales automation
+        Sales team CRM
+        Lead distribution system
+        Reassign leads CRM
+        CRM sales tools
+        Best CRM tool to assign leads to salespeople
+        Automatically assign leads to sales reps in CRM
+        CRM for managing large sales teams
+        Assigning leads to sales reps made easy
+        How to balance lead workload in CRM
+        Efficient sales rep lead management
+        Manage high volume leads in CRM
+        Distribute CRM leads to sales team members
+        Enterprise lead assignment software
+        CRM for distributed sales teams
+        High-volume lead management CRM
+        Inside sales CRM automation
+        Real-time lead distribution
+        Multi-user CRM with lead assignment
+        Sales lead routing for B2B CRM
+        Avoid manual lead assignment
+        Prevent lead overload on sales reps
+        Fix uneven sales lead distribution
+        Simplify CRM lead routing
+        Save time on CRM lead assignments
+        Reduce CRM admin workload
+        Improve CRM efficiency for sales managers
+        """,
+    'description': """
+        CRM Lead
+        Crm
+        Lead
+        Assign
+        Salesperson
+        Salesperson Assign
+        Assign Salesperson
+        Assign salesperson CRM
+        Bulk assign leads CRM
+        CRM salesperson assignment tool
+        Mass lead reassignment
+        CRM lead management
+        Salesperson lead distribution
+        CRM automation for sales team
+        Lead assignment CRM
+        CRM sales automation
+        Sales team CRM
+        Lead distribution system
+        Reassign leads CRM
+        CRM sales tools
+        Best CRM tool to assign leads to salespeople
+        Automatically assign leads to sales reps in CRM
+        CRM for managing large sales teams
+        Assigning leads to sales reps made easy
+        How to balance lead workload in CRM
+        Efficient sales rep lead management
+        Manage high volume leads in CRM
+        Distribute CRM leads to sales team members
+        Enterprise lead assignment software
+        CRM for distributed sales teams
+        High-volume lead management CRM
+        Inside sales CRM automation
+        Real-time lead distribution
+        Multi-user CRM with lead assignment
+        Sales lead routing for B2B CRM
+        Avoid manual lead assignment
+        Prevent lead overload on sales reps
+        Fix uneven sales lead distribution
+        Simplify CRM lead routing
+        Save time on CRM lead assignments
+        Reduce CRM admin workload
+        Improve CRM efficiency for sales managers
+        
+    """,
+    'author': "TechUltra Solutions Private Limited",
+    'company': 'TechUltra Solutions Private Limited',
+    'website': "https://www.techultrasolutions.com/",
+    'depends': ['crm'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/assign_salesperson_wizard.xml',
+    ],
+    'images': [
+        'static/description/banner.jpg',
+        'static/description/main_screen.gif'
+    ],
+
+    'installable': True,
+    'application': True,
+    'license': 'OPL-1',
+}
