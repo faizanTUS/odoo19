1d845e60e3f5dda389395c378443ee2a0f92f390
Faizanali <faizan.techultrasolutions@gmail.com>
2026-02-27 16:36:40 +0530

    [ADD] changes in 18

--- riyan/v17/purchase_quotation_from_crm_advanced/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
-# -*- coding: utf-8 -*-
-from . import res_config_settings
-from . import crm_lead
-from . import purchase_order
-from . import crm_lead_purchase_line
