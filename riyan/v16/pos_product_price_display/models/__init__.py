d355dde3aa7e8f8db9f7dec0fa183a038fa597c0
riyanTUS <riyanshaikh.techultra@gmail.com>
2026-03-05 13:27:26 +0530

    [ADD] Added pos_product_price_display module in version 16

---
+++ riyan/v16/pos_product_price_display/models/__init__.py
@@ -0,0 +1,5 @@
+# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+from . import pos_config
+from . import pos_session
+from . import res_config_settings
