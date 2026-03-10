2b81275e1b5d6876e06873510fa822ff2471cc39
Faizanali <faizan.techultrasolutions@gmail.com>
2026-03-10 11:27:31 +0530

    [ADD] remove module

--- riyan/v16/pos_product_price_display_pro/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
-# -*- coding: utf-8 -*-
-from . import pos_config
-from . import pos_session
-from . import res_config_settings
--- riyan/v16/print_action_buttons_access/__init__.py
+++
@@ -1,1 +0,0 @@
-# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
