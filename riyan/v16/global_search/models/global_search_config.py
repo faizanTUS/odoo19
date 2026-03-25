951d3851b3c4a24e0d97f124573251164285c563
riyanTUS <riyanshaikh.techultra@gmail.com>
2026-03-25 14:35:11 +0530

    [ADD] Added global_search module in version 16

---
+++ riyan/v16/global_search/models/global_search_config.py
@@ -0,0 +1,20 @@
+# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    global_search_model_ids = fields.Many2many(
+        comodel_name="ir.model",
+        relation="res_users_global_search_model_rel",
+        column1="user_id",
+        column2="model_id",
+        string="Global Search Models",
+        domain=[
+            ("is_mail_thread", "=", True),
+            ("is_mail_activity", "=", True),
+        ],
+        help="Models this user can search in the global search. "
+             "Leave empty to search all mail-thread + mail-activity models (default).",
+    )
