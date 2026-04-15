2a3d31fbb1d504c7fb058dbc65082714f581f272
riyanTUS <riyanshaikh.techultra@gmail.com>
2026-04-15 10:48:08 +0530

    [ADD] Added advanced_access_control module in version 18

---
+++ riyan/v18/advanced_access_control/models/__init__.py
@@ -0,0 +1,9 @@
+# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+from . import advanced_access_form_button
+from . import advanced_access_form_notebook_page
+from . import advanced_access_policy
+from . import advanced_access_service
+from . import ir_ui_menu
+from . import ir_http
+from . import models_base
