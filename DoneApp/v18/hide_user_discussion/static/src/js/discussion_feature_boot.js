14a21a8551f1f62ebb2f134be9b10e6b275d5aab
riyanTUS <riyanshaikh.techultra@gmail.com>
2026-05-12 12:39:29 +0530

    [MOD] Updated a hide_user_discussion module in version 18

---
+++ DoneApp/v18/hide_user_discussion/static/src/js/discussion_feature_boot.js
@@ -0,0 +1,36 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { registry } from "@web/core/registry";
+import { session } from "@web/session";
+import { ChatHub } from "@mail/core/common/chat_hub";
+import { Notification } from "@web/core/notifications/notification";
+import { OutOfFocusService } from "@mail/core/common/out_of_focus_service";
+
+if (!session.discussion_enabled) {
+    const systray = registry.category("systray");
+    if (systray.contains("mail.messaging_menu")) {
+        systray.remove("mail.messaging_menu");
+    }
+}
+
+patch(ChatHub.prototype, {
+    get discussUiAllowed() {
+        return Boolean(session.discussion_enabled);
+    },
+});
+
+patch(Notification.prototype, {
+    get isDiscussionEnabled() {
+        return Boolean(session.discussion_enabled);
+    },
+});
+
+patch(OutOfFocusService.prototype, {
+    notify() {
+        if (!session.discussion_enabled) {
+            return;
+        }
+        return super.notify(...arguments);
+    },
+});
