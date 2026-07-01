afbce395d5c1abc21436675bba4d53115de7018e
trupti-techultra <truptipanchal.techultra@gmail.com>
2026-07-01 18:47:25 +0530

    [ADD] Added modules

---
+++ incomplete/riyan/16/v16c/product_attribute_restriction/models/__init__.py
@@ -0,0 +1,6 @@
+# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
+
+from . import res_users
+from . import product_attribute
+from . import product_attribute_value
+from . import product_template_attribute_line
