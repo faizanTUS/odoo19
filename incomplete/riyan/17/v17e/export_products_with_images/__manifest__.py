909727d77c398eb9598fb97f1b0bede0078dce17
trupti-techultra <truptipanchal.techultra@gmail.com>
2026-07-01 18:12:43 +0530

    [Add] Added export_products_with_images module incompleted

---
+++ incomplete/riyan/17/v17e/export_products_with_images/__manifest__.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+{
+    'name': 'Product Data & Image Export | XLSX Product Report | Odoo Products',
+    'version': '17.0.0.0',
+    'category': 'Sales',
+    'summary': """Export products data with images in Excel format
+                Odoo Export Product with Images
+                Export Product Data Odoo
+                Odoo Product Excel Export
+                Export Product XLSX Odoo
+                Odoo Export Product Attributes
+                Product Image Export Odoo
+                Odoo Product Data Report
+                Odoo Export Product List Excel
+                Product Master Export Odoo
+                Export Products with Pictures Odoo
+                Export Odoo product data with images to Excel
+                Odoo module to export product catalog
+                Export multiple products from Odoo with images
+                Odoo export product data to xlsx file
+                Excel export of product attributes Odoo
+                Export product variants and images Odoo
+                Odoo product sheet with images download
+                Product listing export tool for Odoo
+                Product image and data export utility Odoo
+                One-click product export Odoo
+                Odoo inventory export
+                Product catalog generation Odoo
+                Odoo data export automation
+                Odoo product management tools
+                Export product master data Odoo
+                Structured product export Odoo
+                Odoo xlsx report with product images
+                Product list downloader Odoo
+                Odoo product sheet generator
+                Data extraction tool for Odoo products
+    """,
+    'author': 'TechUltra Solutions Private Limited',
+    'company': 'TechUltra Solutions Private Limited',
+    'website': "https://www.techultrasolutions.com/",
+    'depends': ['base', 'product'],
+    'description': """
+        The Export Product Data with Images module enables users to generate .xlsx files for products using
+        the product of the company's in Odoo. It supports exporting data for single or multiple products,
+        including essential details such as product images and key attributes. Simplify product data management
+        with structured Excel reports in just a few clicks!
+        Odoo Export Product with Images
+        Export Product Data Odoo
+        Odoo Product Excel Export
+        Export Product XLSX Odoo
+        Odoo Export Product Attributes
+        Product Image Export Odoo
+        Odoo Product Data Report
+        Odoo Export Product List Excel
+        Product Master Export Odoo
+        Export Products with Pictures Odoo
+        Export Odoo product data with images to Excel
+        Odoo module to export product catalog
+        Export multiple products from Odoo with images
+        Odoo export product data to xlsx file
+        Excel export of product attributes Odoo
+        Export product variants and images Odoo
+        Odoo product sheet with images download
+        Product listing export tool for Odoo
+        Product image and data export utility Odoo
+        One-click product export Odoo
+        Odoo inventory export
+        Product catalog generation Odoo
+        Odoo data export automation
+        Odoo product management tools
+        Export product master data Odoo
+        Structured product export Odoo
+        Odoo xlsx report with product images
+        Product list downloader Odoo
+        Odoo product sheet generator
+        Data extraction tool for Odoo products
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_template_views.xml',
+        'wizard/export_product_data_wizard.xml',
+    ],
+    "images": [
+        "static/description/main_screen.gif"
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+    'price': 11.00,
+}
