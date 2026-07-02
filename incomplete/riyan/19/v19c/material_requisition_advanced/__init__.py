0fb15d5cf7843030e2e4b11a5fa2680662f0022f
trupti-techultra <truptipanchal.techultra@gmail.com>
2026-07-02 10:56:06 +0530

    [ADD] Added incompleted module in v19

---
+++ incomplete/riyan/19/v19c/material_requisition_advanced/__init__.py
@@ -0,0 +1,6 @@
+# Part of Techultra Solutions. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import wizard
+from . import report
