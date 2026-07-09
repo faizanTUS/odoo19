203e3bb4f8ece49f00b3d56def6da5fd2ff10429
RiyanTUS <riyanshaikh.techultra@gmail.com>
2026-07-09 17:03:34 +0530

    [ADD] Added tus_invoice_picking module in all version

---
+++ DoneApp/v17/tus_invoice_picking/models/account_move.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    picking_id = fields.Many2one('stock.picking', string='Related Picking', help="Reference to the related picking.")
