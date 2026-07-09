3196e1b82f6392be262955d0b49adcff80512214
RiyanTUS <riyanshaikh.techultra@gmail.com>
2026-07-09 17:59:12 +0530

    [ADD] Added tus_show_optional_columns module in all version

---
+++ DoneApp/v19/tus_show_optional_columns/models/res_config_settings.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    is_optional_view = fields.Boolean(
+        string="Enable Optional Tree View",
+        config_parameter="tus_show_optional_fields.is_optional_view",
+    )
