be5166420153cbba76b6544642ec7127d88ffbb9
RiyanTUS <riyanshaikh.techultra@gmail.com>
2026-07-09 17:40:55 +0530

    [ADD] Added tus_ecommerce_barcode_search module in all version

---
+++ DoneApp/v18/tus_ecommerce_barcode_search/__manifest__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+{
+    'name': 'eCommerce Barcode Product Search | Website Product Lookup | Odoo Webshop Search',
+    'version': '18.0.0.0',
+    'category': 'Website',
+    'summary': """
+    Enable quick and accurate product searches on your eCommerce website using barcode numbers. This feature allows customers and staff to find product details instantly by scanning or entering a barcode.
+    Barcode product search
+    Odoo eCommerce barcode search
+    Search product by barcode
+    Barcode lookup on website
+    Barcode search module for Odoo
+    Barcode-based product search
+    Search products on eCommerce site using barcode
+    Scan barcode to find product details
+    Odoo eCommerce search by barcode number
+    Barcode scanning for online product search
+    Website product search with barcode support
+    Find products using barcode on online store
+    Barcode product lookup Odoo
+    Online store barcode scanner
+    Odoo website search enhancement
+    Barcode search feature for customers
+    Barcode input in eCommerce search bar
+    """,
+    'description': """
+    This module enhances your eCommerce platform by enabling barcode-based product search functionality. Users can simply enter or scan a product’s barcode number into the search bar to instantly access its details.
+    Whether used by customers for quick self-service or by staff for efficient product lookup, this feature streamlines navigation and improves the overall user experience—especially in inventory-heavy businesses.
+    Perfect for industries where products are primarily tracked by barcodes, this tool ensures faster access to product information and reduces search errors.
+    
+    Barcode product search
+    Odoo eCommerce barcode search
+    Search product by barcode
+    Barcode lookup on website
+    Barcode search module for Odoo
+    Barcode-based product search
+    Search products on eCommerce site using barcode
+    Scan barcode to find product details
+    Odoo eCommerce search by barcode number
+    Barcode scanning for online product search
+    Website product search with barcode support
+    Find products using barcode on online store
+    Barcode product lookup Odoo
+    Online store barcode scanner
+    Odoo website search enhancement
+    Barcode search feature for customers
+    Barcode input in eCommerce search bar
+        
+    """,
+    'author': "Techultra Solutions Private Limited",
+    'website': "https://www.techultrasolutions.com/",
+    'images': ['static/description/main_screen.gif'],
+    'depends': ['website_sale'],
+    'price': 13.00,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
