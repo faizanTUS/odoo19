51d9e0779da6a8c933be83a393b481cd13471461
RushiTus <rushirathod.techultra@gmail.com>
2026-07-17 11:30:06 +0530

    [ADD] added module in all versions: sample_bag

---
+++ DoneApp/v19/sample_bag/models/__init__.py
@@ -0,0 +1,8 @@
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+from . import sample_bag
+from . import sample_bag_line
+from . import sale_order
+from . import stock_warehouse
+from . import stock_picking
+from . import product_product
+from . import res_config_settings
---
+++ DoneApp/v19/sample_bag/models/stock_warehouse.py
@@ -0,0 +1,13 @@
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+from odoo import fields, models
+
+
+class StockWarehouseInherit(models.Model):
+    _inherit = "stock.warehouse"
+
+    sample_bag_in_type_id = fields.Many2one(
+        "stock.picking.type", string="Sample Bag In Type"
+    )
+    sample_bag_out_type_id = fields.Many2one(
+        "stock.picking.type", string="Sample Bag Out Type"
+    )
